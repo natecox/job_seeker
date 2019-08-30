4d2d66f4a1e8e638245801b6494c520d3539b6d5
Nathan Cox <tsuujin@gmail.com>
2019-08-29 20:02:29 -0700

    Add name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_830_001_339) do
+ActiveRecord::Schema.define(version: 2019_08_30_030142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20_190_830_001_339) do
     t.inet 'last_sign_in_ip'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'name'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
